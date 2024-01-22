09f405a8d36b01a24d7b0e87e03ca8011f981bb9
Aman <ajoshi373@gmail.com>
2024-01-23 00:12:18 +0530

    varibales declaration

--- 02variables/main.go
+++ 02variables/main.go
@@ -3,15 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var username string ="Aman"
+	var username string = "Aman"
 	fmt.Println(username)
-	fmt.Printf("Variable is of type: %T \n",username)
+	fmt.Printf("Variable is of type: %T \n", username)
 
-	var isLoggedIn bool =true
+	var isLoggedIn bool = true
 	fmt.Println(isLoggedIn)
-	fmt.Printf("Variable is of type: %T \n",isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
 
-	var smallValue uint8 =256
+	var smallValue uint8 = 255
 	fmt.Println(smallValue)
-	fmt.Printf("Variable is of type: %T \n",smallValue)
+	fmt.Printf("Variable is of type: %T \n", smallValue)
+
+	//default values and some aliases
+	var anothervariable int
+	fmt.Println(anothervariable)
+	fmt.Printf("Variable is of type:%T \n", anothervariable)
+
+	//implicit type
+	var website = "https://random.com"
+	fmt.Println(website)
+
+	//walrus operator , no var style
+
+	number := 100
+	fmt.Println(number)
 }
