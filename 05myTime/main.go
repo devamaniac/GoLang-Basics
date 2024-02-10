ec468bbf114d3f3744469e2d70e69c97725bed92
devamaniac <amaniac1999@gmail.com>
2024-02-10 16:00:20 +0530

    Command to build for windows,mac,linux

--- 05myTime/main.go
+++ 05myTime/main.go
@@ -9,7 +9,14 @@ func main() {
 	fmt.Println(time.Now())
 
 	presentTime := time.Now()
-	fmt.Println("The date is:", presentTime.Format("01-02-2006"))
+	fmt.Println("The date is:", presentTime.Format("2006-02-01"))
 	fmt.Println("The date and time is:", presentTime.Format("01-02-2006 15:04:05"))
 	fmt.Println("The date and day is:", presentTime.Format("01-02-2006 Monday"))
+
+	//to build the code for linux, mac or window use the below commands
+	/*
+		GOOS="windows" go build
+		GOOS="linux" go build
+		GOOS="darwin" go build
+	*/
 }
