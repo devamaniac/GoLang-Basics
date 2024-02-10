17c3756194e3106e0d0f9a39d4dc2c2ecafd7387
devamaniac <amaniac1999@gmail.com>
2024-02-11 01:25:12 +0530

    added slices code

---
+++ 08slices/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Learning Slices")
+
+	var friends = []string{"A", "B", "C"}
+	fmt.Printf("The type of friends is:%T\n", friends)
+	fmt.Println("The values in slice friends are:", friends)
+
+	friends = append(friends, "D")
+	fmt.Println(friends)
+
+	friends = append(friends[1:])
+	fmt.Println(friends)
+	friends = append(friends[1:3])
+	fmt.Println(friends)
+
+	//syntax for make
+
+	var highscore = make([]int, 4)
+	highscore[0] = 1
+	highscore[1] = 3
+	highscore[2] = 4
+	highscore[3] = 2
+
+	fmt.Println("the Highscore are:", highscore)
+	//highscore[4]=5 will give error index out of bounds
+
+	//but if we use append a new memory allocation takes place and we can add
+	//any number of values to our slice
+	sort.Ints(highscore)
+	fmt.Println("the Highscore in sorted form are:", highscore)
+	fmt.Println(sort.IntsAreSorted(highscore))
+
+}
